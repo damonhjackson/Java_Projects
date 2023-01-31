83bd2fa4ab28916449407d25f38a3098e60746c7
Damon Jackson <78815594+damonhjackson@users.noreply.github.com>
2023-01-30 23:51:43 -0600

    Primitive types

--- main.java
+++ main.java
@@ -1,8 +1,14 @@
 public class main {
     public static void main(String[] args) {
-        // declaring and initalizing variables
-        int myAge = 30;
-        int herAge = myAge;
-        System.out.println(herAge);
+        // Primitive types
+        byte age = 30;
+        // Determine the number of times video was watched
+        // in java can use an underscore every 3 numbers like a coma
+
+        //int viewsCount = 3_123_456_789;
+        long viewsCount = 3_123_456_789L;  //define long integer
+        float price = 10.99F; // define a float
+        char letter = 'A';
+        boolean isEligible = false;
     }
 }
