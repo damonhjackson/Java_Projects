27d1cd060d2a48f2a61a9cf111c9c1f2e9bd529f
Damon Jackson <78815594+damonhjackson@users.noreply.github.com>
2023-01-31 00:13:19 -0600

    Reference Types

--- main.java
+++ main.java
@@ -1,14 +1,10 @@
+import java.util.Date;
+
 public class main {
     public static void main(String[] args) {
-        // Primitive types
+        // Reference Types
         byte age = 30;
-        // Determine the number of times video was watched
-        // in java can use an underscore every 3 numbers like a coma
-
-        //int viewsCount = 3_123_456_789;
-        long viewsCount = 3_123_456_789L;  //define long integer
-        float price = 10.99F; // define a float
-        char letter = 'A';
-        boolean isEligible = false;
+        Date now = new Date(); //Date
+        System.out.println(now);
     }
 }
