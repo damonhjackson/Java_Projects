c8f1285fd112c209892d94feed73d7fc92019c6e
Damon Jackson <78815594+damonhjackson@users.noreply.github.com>
2023-01-31 00:23:21 -0600

    Strings

--- main.java
+++ main.java
@@ -2,9 +2,14 @@ import java.util.Date;
 
 public class main {
     public static void main(String[] args) {
-        // Reference Types
-        byte age = 30;
-        Date now = new Date(); //Date
-        System.out.println(now);
+        // Strings
+        // concatinate or join string using +
+        String message = "Hello World" + "!!";
+
+        System.out.println(message); // String literal
+        System.out.println(message.startsWith("!!"));
+        System.out.println(message.endsWith("!!")); //check to see what string ends with
+        System.out.println(message.length()); // check the length of string
+
     }
 }
