9ffafe9133bc9ee9b49fa0a6a39d96220952265a
Damon Jackson <78815594+damonhjackson@users.noreply.github.com>
2023-01-31 00:35:09 -0600

    Arrays, define and print values

--- main.java
+++ main.java
@@ -1,15 +1,21 @@
+import java.util.Arrays;
 import java.util.Date;
 
 public class main {
     public static void main(String[] args) {
-        // Strings
-        // concatinate or join string using +
-        String message = "Hello World" + "!!";
+       // Arrays, how to define and print old way
+        int [] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
 
-        System.out.println(message); // String literal
-        System.out.println(message.startsWith("!!"));
-        System.out.println(message.endsWith("!!")); //check to see what string ends with
-        System.out.println(message.length()); // check the length of string
+        System.out.println(Arrays.toString(numbers)); // print array values
+
+        // Array, define and print new way
+        int [] numbers2 = {2, 3, 5, 1, 4};
+
+        Arrays.sort(numbers2); // sort array
+        System.out.println(numbers.length); // number of items in array
+        System.out.println(Arrays.toString(numbers2));
 
     }
 }
