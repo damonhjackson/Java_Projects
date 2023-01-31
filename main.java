b8778b47fff511b7a2d775b82c0880489b727566
Damon Jackson <78815594+damonhjackson@users.noreply.github.com>
2023-01-31 15:23:25 -0600

    reading input

--- main.java
+++ main.java
@@ -1,19 +1,13 @@
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-       // Multi-dimensional Arrays
-        // row and columns
-        //two dimensional array
-        int [] [] numbers = new int[2][3];
-        numbers[0][0] = 1;
-        System.out.println(Arrays.deepToString(numbers));
-
-        // two dimensinal array
-        int [][] numbers2 = {{1,2,3}, {4,5,6}};
-        System.out.println(Arrays.deepToString(numbers2));
-
-
+       // reading input, get user age
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Age: ");
+        byte age = scanner.nextByte();
+        System.out.println("you are " + age);
     }
 }
